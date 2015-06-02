8d7a55872cfbe487f60f9f988eab3930e8543e48
Alexander <abumov.alex@gmail.com>
2015-06-02 17:23:19 +0300

    Add listeners

--- app/src/main/java/com/alex/abumov/myappportfolio/MainActivity.java
+++ app/src/main/java/com/alex/abumov/myappportfolio/MainActivity.java
@@ -1,17 +1,58 @@
 package com.alex.abumov.myappportfolio;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+
+    Context app_context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Variables */
+        app_context = getApplicationContext();
+
+        // ----------------------------------------------------------------
+        // ----------------------------------------------------------------
+
+        /* Buttons */
+        Button mna_button_spotify = (Button) findViewById(R.id.mna_button_spotify);
+        Button mna_button_scores = (Button) findViewById(R.id.mna_button_scores);
+        Button mna_button_library = (Button) findViewById(R.id.mna_button_library);
+        Button mna_button_bigger = (Button) findViewById(R.id.mna_button_bigger);
+        Button mna_button_bacon = (Button) findViewById(R.id.mna_button_bacon);
+        Button mna_button_capstone = (Button) findViewById(R.id.mna_button_capstone);
+
+        // ----------------------------------------------------------------
+        // ----------------------------------------------------------------
+
+        /* Listeners */
+        View.OnClickListener testListener = new View.OnClickListener() {
+            public void onClick(View v) {
+                testToast();
+            }
+        };
+        mna_button_spotify.setOnClickListener(testListener); // mna_button_spotify listener
+        // ----------------------------------------------------------------
+        mna_button_scores.setOnClickListener(testListener); // mna_button_scores listener
+        // ----------------------------------------------------------------
+        mna_button_library.setOnClickListener(testListener); // mna_button_library listener
+        // ----------------------------------------------------------------
+        mna_button_bigger.setOnClickListener(testListener); // mna_button_bigger listener
+        // ----------------------------------------------------------------
+        mna_button_bacon.setOnClickListener(testListener); // mna_button_bacon listener
+        // ----------------------------------------------------------------
+        mna_button_capstone.setOnClickListener(testListener); // mna_button_capstone listener
     }
 
     @Override
@@ -34,5 +75,10 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void testToast() {
+        CharSequence text = "This button will launch my capstone project!";
+        Toast.makeText(app_context, text, Toast.LENGTH_SHORT).show();
     }
 }
